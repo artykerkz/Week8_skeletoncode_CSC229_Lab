8317df592ee7970ad667804b558449c5b6751f89
artykerkz <161413471+artykerkz@users.noreply.github.com>
2025-10-14 20:53:27 -0400

    Update BinarySearch.java

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
@@ -29,5 +29,22 @@ public class BinarySearch {
     }
     //ToDo 2: Call the above method and test the algorithm  
     // provide time and space analysis 
+public static void main(String[] args) {
+    int [] sortedArray = {3, 5, 9, 13, 18, 23, 27, 29, 32};
+    int key = 13;
 
+    int result = runBinarySearchIteratively(sortedArray, key, 0, sortedArray.length - 1); 
+
+    if (result == Integer.MAX_VALUE) {
+        System.out.println("Key not found"); 
+    } else {
+        System.out.println("Key: " + key + " found at index: " + result);
+    }
 }
+}
+
+/*
+Time complexity: O(log n) because it grows logarithmically each time. 
+Space complexity: O(1) because it needs a constant amount of extra memory regardless of array size.
+*/
+
