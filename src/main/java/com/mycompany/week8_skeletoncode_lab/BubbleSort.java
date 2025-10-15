ac00d0c82b776f4cb2969c3eeb8d96ab7c23ac1a
artykerkz <161413471+artykerkz@users.noreply.github.com>
2025-10-14 21:02:53 -0400

    Update BubbleSort.java

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
@@ -15,9 +15,32 @@ public class BubbleSort {
         for (outer = size - 1; outer > 0; outer--) { // counting down
             for (inner = 0; inner < outer; inner++) { // bubbling up
                 //ToDo 3: complete this algorithm, test it, provide its time complexity
+                if (a[inner] > a[inner + 1]) { 
+                    temp = a[inner];
+                    a[inner] = a[inner + 1];
+                    a[inner + 1] = temp;; 
+        
             }
         }
     }
-    
-    
 }
+
+ public static void main(String[] args) {
+        int[] numbers = {3, 7, 1, 5, 9};
+        System.out.println("Before sorting:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        bubbleSort(numbers, numbers.length);
+
+        System.out.println("\nAfter sorting:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+ }
+}
+
+/*
+Time complexity: O(n^2) because the outer loop is n and the inner loop is n, which is n * n.
+*/
