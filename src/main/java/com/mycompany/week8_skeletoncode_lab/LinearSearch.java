90cc963abbd308676ef920dd13f54b7aa8575141
artykerkz <161413471+artykerkz@users.noreply.github.com>
2025-10-14 21:13:51 -0400

    Update LinearSearch.java

--- src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.week8_skeletoncode_lab;
+
 
 /**
  *
@@ -15,6 +15,34 @@ public class LinearSearch {
         int n = arr.length;
         // Todo 01: - complete the implementation of linear search and test your code  
          //         - prvoide asymptotic analysis of the developed solution
+    
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return i;
+        }
     }
     
+    return -1;
+        
+    }
+public static void main(String[] args) {
+    int[] numbers = {3, 0, 5, 8, 6, 9, 1, 7, 2, 4};
+    int key = 3;
+    
+    int result = search(numbers, key);
+    
+    if (result == -1) {
+        System.out.println("Key not found"); 
+    } else {
+        System.out.println("Key " + key + " found at index: " + result);
+    }
+}    
+    
 }
+
+/*
+Asymptotic analysis:
+best case: O(1) if the key is in the first element.
+worst case: O(n) if the key is in the last element or doesn't exist.
+average case: O(n) since about half of the numbers get checked on average.
+*/
